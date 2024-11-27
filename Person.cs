5ad3b43a65b6cd6174099b5a97634e01229ad8ad
dc91_ <dc91_@hotmail.com>
2024-11-27 10:07:09 +0100

    Ex Complete Verison 1

--- Person.cs
+++ Person.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ExWeek2
 {
     internal class Person
     {
-        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Person(string firstName, string lastName, int age) 
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public string Print()
+        {
+            return $"{FirstName} {LastName} is {Age} years old";
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using ExWeek2;
 
+Console.WriteLine("Ading books to list and printing:");
 Book book1 = new Book("J. K. Rowling", "Harry Potter and the Sorcerer\'s Stone", 223);
 Book book2 = new Book("Dilan Chroscik", "This Repo", 10);
 
@@ -8,4 +9,15 @@ List<Book> books = [book1, book2];
 foreach (var book in books)
 {
     Console.WriteLine(book.Print());
+}
+
+Console.WriteLine("\nAdding people to list and printing:");
+Person person1 = new Person("Dilan", "Chroscik", 33);
+Person person2 = new Person("Emina", "Hozo", 30);
+
+List<Person> persons = [person1, person2];
+
+foreach (var person in persons)
+{
+    Console.WriteLine(person.Print());
 }
